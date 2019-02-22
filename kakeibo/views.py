a8a4c0d16a20f88d6d8a65ea1e50433e0ddef724
yasutadak <yasutada629@gmail.com>
2019-02-22 21:31:00 +0900

    add_views.py
    一覧表示用のビューの設定

--- kakeibo/views.py
+++ kakeibo/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
+from .models import Category, Kakeibo
 # Create your views here.
+
+class KakeiboListView(ListView):
+  model = Kakeibo
+  template_name = 'kakeibo/kakeibo_list.html'
+
+  def queryset(self):
+    return Kakeibo.objects.all()
