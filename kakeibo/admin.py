2d4b6380ce4a97c7ae7c5f3dd36ee4a1fc5327fc
yasutadak <yasutada629@gmail.com>
2019-02-22 21:24:09 +0900

    create_table
    categoryテーブルとkakeiboテーブルの作成

--- kakeibo/admin.py
+++ kakeibo/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Category, Kakeibo
 # Register your models here.
+
+class KakeiboAdmin(admin.ModelAdmin):
+  list_display=('date', 'category', 'money', 'memo')
+
+admin.site.register(Category)
+admin.site.register(Kakeibo, KakeiboAdmin)
--- kakeibo/models.py
+++ kakeibo/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Category(models.Model):
+  class Meta:
+    db_table = "category"
+    verbose_name = "カテゴリ"
+    verbose_name_plural = "カテゴリ"
+
+  category_name = models.CharField(max_length=255, unique=True)
+
+  def __str__(self):
+    return self.category_name
+
+class Kakeibo(models.Model):
+  class Meta:
+    db_table = "kakeibo"
+    verbose_name = "家計簿"
+    verbose_name_plural = "家計簿"
+
+  date = models.DateField(verbose_name="日付",default=datetime.now)
+  category = models.ForeignKey(Category, on_delete = models.PROTECT, verbose_name="カテゴリ")
+  money = models.IntegerField(verbose_name="金額", help_text="単位は日本円")
+  memo = models.CharField(verbose_name="メモ", max_length=500)
+
+  def __str__(self):
+    return self.memo
