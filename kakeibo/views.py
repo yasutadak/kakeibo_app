e459c3457ea8eca298b256c2495e3bdb961833c0
yasutadak <yasutada629@gmail.com>
2019-02-23 14:58:33 +0900

    create_kakeibo_form.html
    データ登録画面の作成

--- kakeibo/views.py
+++ kakeibo/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from . forms import KakeiboForm
+from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 from .models import Category, Kakeibo
 # Create your views here.
@@ -9,3 +11,15 @@ class KakeiboListView(ListView):
 
   def queryset(self):
     return Kakeibo.objects.all()
+
+class KakeiboCreateView(CreateView):
+  #利用するモデルを指定
+  model = Kakeibo
+  #利用するフォームクラス名を指定
+  form_class = KakeiboForm
+  #登録処理が正常終了した場合の遷移先を指定
+  success_url = reverse_lazy('kakeibo:create_done')
+
+def create_done(request):
+  #登録処理が正常終了した場合に呼ばれるテンプレートを指定
+  return render(request, 'kakeibo/create_done.html')
