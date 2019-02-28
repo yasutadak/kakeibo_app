0e32e5f40bbc2ca976e1022457cd87a59de6f7ca
yasutadak <yasutada629@gmail.com>
2019-02-28 12:21:30 +0900

    edit_views.py
    matrix_list変数にデータがない月のカテゴリが0円で補完された月毎＆カテゴリ毎の合計金額データセットを格納

--- kakeibo/views.py
+++ kakeibo/views.py
@@ -97,3 +97,33 @@ def show_line_graph(request):
       money = category_total[j]['total_price']
       category = Category.objects.get(pk=category_total[j]['category'])
       monthly_sum_data.append([x_label[i], category.category_name,money])
+  #折れ線グラフのボーダーカラー色の設定
+  border_color_list = ['254,97,132,0.8','54,164,235,0.8','0,255,65,0.8','255,241,15,0.8',\
+                      '255,94,25,0.8','84,77,203,0.8','204,153,50,0.8','214,216,165,0.8',\
+                      '33,30,45,0.8','52,38,89,0.8']
+  border_color = []
+  for x,y in zip(category_list, border_color_list):
+    border_color.append([x,y])
+
+  background_color_list=['254,97,132,0.5','54,164,235,0.5','0,255,65,0.5','255,241,15,0.5',\
+                          '255,94,25,0.5','84,77,203,0.5','204,153,50,0.5','214,216,165,0.5'\
+                          '33,30,45,0.5','52,38,89,0.5']
+  background_color = []
+  for x,y in zip(category_list, background_color_list):
+    background_color.append([x,y])
+
+  #月毎&カテゴリ毎の合計金額データを生成する
+  #カテゴリが登録されていない月の合計金額は0にセットする
+  #まず、全日付×全カテゴリ×合計金額[0]のリストを生成する
+  matrix_list = []
+  for item_label in x_label:
+    for item_category in category_list:
+      matrix_list.append([item_label, item_category, 0])
+  """matrix_listとmonthlysum_dataに対して、「年月+カテゴリ」の
+   組み合わせが一致する要素に対してmatrix_listの金額（０円）を
+   monthly_sum_dataの金額で上書きする。
+   """
+   for yyyy_mm,category,total in monthly_sum_data:
+    for i,data in enumerate(matrix_list):
+      if data[0]==yyyy_mm and data[1]==category:
+        matrix_list[i][2] = total
