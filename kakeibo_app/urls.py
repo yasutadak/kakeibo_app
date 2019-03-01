604442ea2d87f2554b5e1968d489c5fe79bdf7f5
yasutadak <yasutada629@gmail.com>
2019-03-01 15:26:56 +0900

    cteate_templates/base.html

--- kakeibo_app/urls.py
+++ kakeibo_app/urls.py
@@ -20,4 +20,5 @@ from django.conf.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('kakeibo/', include('kakeibo.urls')),
+    path('acounts/', include('django.contrib.auth.urls')),
 ]
