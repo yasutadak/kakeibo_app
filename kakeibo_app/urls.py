ebf56fde4e2405b4d5772c6511b5cc99c294b1e8
yasutadak <yasutada629@gmail.com>
2019-03-01 16:26:01 +0900

    edit_urls
    URL設定

--- kakeibo_app/urls.py
+++ kakeibo_app/urls.py
@@ -20,5 +20,6 @@ from django.conf.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('kakeibo/', include('kakeibo.urls')),
-    path('acounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
